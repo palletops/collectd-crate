339979b7564dbf6c21a56af6b9c03ca554ad6c6c
Hugo Duncan <hugo@hugoduncan.org>
2013-04-19 12:07:15 -0400

    Fix dev profiles

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,8 @@
 {:dev
- {:dependencies [[com.palletops/pallet "0.8.0-beta.8" :classifier "tests"]
+ {:dependencies [[com.palletops/pallet "0.8.0-beta.7" :classifier "tests"]
                  [com.palletops/crates "0.1.0"]
                  [ch.qos.logback/logback-classic "1.0.9"]]
-  :plugins [[com.palletops/pallet-lein "0.6.0-beta.7"]
+  :plugins [[com.palletops/pallet-lein "0.6.0-beta.9"]
             [codox/codox.leiningen "0.6.4"]
             [lein-marginalia "0.7.1"]
             [lein-resource "0.3.2"]]
@@ -20,11 +20,12 @@
                  [lein-marginalia "0.7.1"]]
        :codox {:writer codox-md.writer/write-docs
                :output-dir "doc/0.8/api"
-               :src-dir-uri "https://github.com/pallet/riemann-crate/blob/develop"
+               :src-dir-uri "https://github.com/pallet/collectd-crate/blob/develop"
                :src-linenum-anchor-prefix "L"}
        :aliases {"marg" ["marg" "-d" "doc/0.8/annotated"]
                  "codox" ["doc"]
                  "doc" ["do" "codox," "marg"]}}
+ :no-checkouts {:checkout-shares ^:replace []} ; disable checkouts
  :release
  {:plugins [[lein-set-version "0.3.0"]]
   :set-version
