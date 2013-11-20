90224fc614196116dd0d0f1739ba85a47af0238e
Hugo Duncan <hugo@hugoduncan.org>
2013-11-20 13:07:07 -0500

    Update to latest pallet 0.8.0-RC.4

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/palletops/collectd-crate"
   :license {:name "All rights reserved"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.palletops/pallet "0.8.0-beta.7"]]
+                 [com.palletops/pallet "0.8.0-RC.4"]]
   :resource {:resource-paths ["doc-src"]
              :target-path "target/classes/pallet_crate/collectd_crate/"
              :includes [#"doc-src/USAGE.*"]}
