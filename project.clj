e6c26d888f5018b4cfd54220673d82411e0b36a0
Hugo Duncan <hugo@hugoduncan.org>
2013-11-20 14:17:48 -0500

    Updated project.clj, release notes and readme for 0.8.0-alpha.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/collectd-crate "0.8.0-SNAPSHOT"
+(defproject com.palletops/collectd-crate "0.8.0-alpha.2"
   :description "Crate for collectd installation"
   :url "http://github.com/palletops/collectd-crate"
   :license {:name "All rights reserved"}
