1bd09b4c7d8c3364123f9f223cda677437bcf7c0
Antoni Batchelli <tbatchelli@acm.org>
2014-04-18 21:17:25 -0700

    Update licenses and remove release boilerplate in prep for lein-pallet-release

--- profiles.clj
+++ profiles.clj
@@ -1,37 +1,10 @@
 {:dev
- {:dependencies [[com.palletops/pallet "0.8.0-RC.4" :classifier "tests"]
+ {:dependencies [[com.palletops/pallet "0.8.0-RC.9" :classifier "tests"]
                  [com.palletops/crates "0.1.1"]
                  [ch.qos.logback/logback-classic "1.0.9"]]
   :plugins [[com.palletops/pallet-lein "0.8.0-alpha.1"]
-            [com.palletops/lein-pallet-crate "0.1.0"]
-            [lein-set-version "0.3.0"]
-            [lein-resource "0.3.2"]
-            [codox/codox.leiningen "0.6.4"]
-            [lein-marginalia "0.7.1"]]
-  :aliases {"live-test-up"
-            ["pallet" "up"
-             "--phases" "install,configure,test"
-             "--selector" "live-test"]
-            "live-test-down" ["pallet" "down" "--selector" "live-test"]
-            "live-test" ["do" "live-test-up," "live-test-down"]}}
- :doc {:dependencies [[com.palletops/pallet-codox "0.1.0"]]
-       :plugins [[codox/codox.leiningen "0.6.4"]
-                 [lein-marginalia "0.7.1"]]
-       :codox {:writer codox-md.writer/write-docs
-               :output-dir "doc/0.8/api"
-               :src-dir-uri "https://github.com/pallet/collectd-crate/blob/develop"
-               :src-linenum-anchor-prefix "L"}
-       :aliases {"marg" ["marg" "-d" "doc/0.8/annotated"]
-                 "codox" ["doc"]
-                 "doc" ["do" "codox," "marg"]}}
- :no-checkouts {:checkout-deps-shares ^:replace []} ; disable checkouts
- :release
- {:plugins [[lein-set-version "0.3.0"]]
-  :set-version
-  {:updates [{:path "README.md"
-              :no-snapshot true
-              :search-regex
-              #"com.palletops/collectd-crate \"\d+\.\d+\.\d+\""}]}}
+            [lein-resource "0.3.2"]]}
+  :no-checkouts {:checkout-deps-shares ^:replace []} ; disable checkouts
  :jclouds {:dependencies [[org.cloudhoist/pallet-jclouds "1.5.2"]
                           [org.jclouds/jclouds-allblobstore "1.5.5"]
                           [org.jclouds/jclouds-allcompute "1.5.5"]
--- project.clj
+++ project.clj
@@ -4,8 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.palletops/pallet "0.8.0-RC.4"]]
-  :resource {:resource-paths ["doc-src"]
-             :target-path "target/classes/pallet_crate/collectd_crate/"
-             :includes [#"doc-src/USAGE.*"]}
-  :prep-tasks ["resource" "crate-doc"])
+                 [com.palletops/pallet "0.8.0-RC.9"]])
