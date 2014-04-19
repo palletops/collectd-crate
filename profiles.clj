15f87ba47d58fd3400c168ece1a97b5b61a5c01e
Antoni Batchelli <tbatchelli@acm.org>
2014-04-18 21:24:45 -0700

    Prepare project for release automation w/lein-pallet-release.

--- profiles.clj
+++ profiles.clj
@@ -1,14 +1,17 @@
-{:dev
- {:dependencies [[com.palletops/pallet "0.8.0-RC.9" :classifier "tests"]
-                 [com.palletops/crates "0.1.1"]
-                 [ch.qos.logback/logback-classic "1.0.9"]]
-  :plugins [[com.palletops/pallet-lein "0.8.0-alpha.1"]
-            [lein-resource "0.3.2"]]}
-  :no-checkouts {:checkout-deps-shares ^:replace []} ; disable checkouts
+{:no-checkouts {:checkout-deps-shares ^{:replace true} []},
  :jclouds {:dependencies [[org.cloudhoist/pallet-jclouds "1.5.2"]
                           [org.jclouds/jclouds-allblobstore "1.5.5"]
                           [org.jclouds/jclouds-allcompute "1.5.5"]
                           [org.jclouds.driver/jclouds-slf4j "1.5.5"
                            :exclusions [org.slf4j/slf4j-api]]
-                          [org.jclouds.driver/jclouds-sshj "1.5.5"]]}
- :vmfest {:dependencies [[com.palletops/pallet-vmfest "0.3.0-beta.2"]]}}
+                          [org.jclouds.driver/jclouds-sshj "1.5.5"]]},
+ :vmfest {:dependencies [[com.palletops/pallet-vmfest "0.3.0-beta.2"]]},
+ :dev {:dependencies [[com.palletops/pallet "0.8.0-RC.9" :classifier "tests"]
+                      [com.palletops/crates "0.1.1"]
+                      [ch.qos.logback/logback-classic "1.0.9"]],
+       :plugins [[lein-pallet-release "0.1.6"]
+                 [com.palletops/pallet-lein "0.8.0-alpha.1"]
+                 [lein-resource "0.3.2"]],
+       :pallet-release
+       {:url "https://pbors:${GH_TOKEN}@github.com/palletops/collectd-crate.git",
+        :branch "master"}}}
